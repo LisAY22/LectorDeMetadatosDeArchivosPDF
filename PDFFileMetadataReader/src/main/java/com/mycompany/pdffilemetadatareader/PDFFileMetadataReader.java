f5df3b797b2ad4c72d49e0016916e73e0bc8a5b0
Karol Castro <karolcastroor0305@gmail.com>
2023-10-05 21:12:01 -0600

    options_window
    Creation of an options window with a grid with two buttons "Ingresar nueva ruta" and "Continuar con la misma ruta"

--- PDFFileMetadataReader/src/main/java/com/mycompany/pdffilemetadatareader/PDFFileMetadataReader.java
+++ PDFFileMetadataReader/src/main/java/com/mycompany/pdffilemetadatareader/PDFFileMetadataReader.java
@@ -1,16 +1,17 @@
 package com.mycompany.pdffilemetadatareader;
-import com.formdev.flatlaf.FlatDarkLaf; // Importa la clase de estilo FlatLaf
+import com.formdev.flatlaf.FlatDarkLaf; // Importar la clase de estilo FlatLaf que se encuentra almacenada de manera local
 
-import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import javax.swing.*;
 
 public class PDFFileMetadataReader {
 
     public static void main(String[] args) {
+        // Establecer FlatLaf como el estilo de la interfaz gráfica
         try {
-            UIManager.setLookAndFeel(new FlatDarkLaf()); // Establece FlatLaf como el estilo de la interfaz gráfica
+            UIManager.setLookAndFeel(new FlatDarkLaf());
         } catch (UnsupportedLookAndFeelException e) {
             e.printStackTrace();
         }
@@ -18,7 +19,7 @@ public class PDFFileMetadataReader {
         JFrame frame = new JFrame("PDF File Metadata Reader");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(500, 400);
-        frame.setResizable(false); // La ventana no es redimensionable
+        frame.setResizable(false);
 
         // Panel para la imagen
         ImageIcon imageIcon = new ImageIcon("C:\\Users\\karol\\Documentos\\GitHub\\LectorDeMetadatosDeArchivosPDF\\PDF file metadata reader.png");
@@ -26,7 +27,6 @@ public class PDFFileMetadataReader {
         imageLabel.setPreferredSize(new Dimension(imageIcon.getIconWidth(), imageIcon.getIconHeight()));
 
         // Panel para el botón
-        JPanel buttonPanel = new JPanel();
         buttonPanel.setOpaque(false); // Hacemos el panel transparente
         buttonPanel.setLayout(new FlowLayout(FlowLayout.RIGHT)); // Alineamos el botón a la derecha
         JButton continueButton = new JButton("Continuar →"); // Agregamos la flecha a la derecha
@@ -36,28 +36,83 @@ public class PDFFileMetadataReader {
         continueButton.setBorderPainted(false); // Quita el borde del botón
         continueButton.setFocusPainted(false); // Deshabilita el efecto de resaltado
         continueButton.setPreferredSize(new Dimension(150, 50)); // Ajusta el tamaño del botón
+        // Ajustar el tamaño del botón
+        continueButton.setPreferredSize(new Dimension(200, 50));
         continueButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                // Lógica cuando se presiona el botón
-                JOptionPane.showMessageDialog(null, "Botón Continuar presionado");
+                frame.dispose();
+                mostrarVentanaOpciones();
             }
         });
         buttonPanel.add(continueButton);
 
-        // Panel principal que contiene el espacio para la imagen y el botón
+        // Panel principal que contiene la imagen y el botón
         JPanel mainPanel = new JPanel();
         mainPanel.setLayout(new BorderLayout());
         mainPanel.add(imageLabel, BorderLayout.CENTER);
         mainPanel.add(buttonPanel, BorderLayout.SOUTH);
 
-        // Agregar el panel principal al marco
         frame.getContentPane().add(mainPanel);
-
-        // Centrar la ventana en la pantalla
         frame.setLocationRelativeTo(null);
-
-        // Hacer visible la ventana
         frame.setVisible(true);
+    }
+
+    private static void mostrarVentanaOpciones() {
+        JFrame ventanaOpciones = new JFrame("Opciones");
+        ventanaOpciones.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        ventanaOpciones.setSize(400, 300);
+        ventanaOpciones.setResizable(false);
+
+        // Panel para los botones
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.fill = GridBagConstraints.BOTH; // Expandir en ambas direcciones
+        gbc.insets = new Insets(10, 10, 10, 10); // Espacio entre botones
+
+        JButton ingresarRutaButton = new JButton("Ingresar nueva ruta");
+        ingresarRutaButton.setBackground(new Color(232, 36, 36));
+        ingresarRutaButton.setForeground(Color.WHITE);
+        ingresarRutaButton.setOpaque(true);
+        ingresarRutaButton.setBorderPainted(false);
+        ingresarRutaButton.setFocusPainted(false);
+        
+        JButton continuarMismaRutaButton = new JButton("Continuar con la misma ruta");
+        continuarMismaRutaButton.setBackground(new Color(232, 36, 36));
+        continuarMismaRutaButton.setForeground(Color.WHITE);
+        continuarMismaRutaButton.setOpaque(true);
+        continuarMismaRutaButton.setBorderPainted(false);
+        continuarMismaRutaButton.setFocusPainted(false);
+
+        // Configurar tamaño preferido para los botones
+        Dimension buttonSize = new Dimension(300, 80);
+        ingresarRutaButton.setPreferredSize(buttonSize);
+        continuarMismaRutaButton.setPreferredSize(buttonSize);
+
+        // Configurar restricciones de cuadrícula para los botones
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        buttonPanel.add(ingresarRutaButton, gbc);
+
+        gbc.gridy = 1;
+        buttonPanel.add(continuarMismaRutaButton, gbc);
+
+        ingresarRutaButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null, "Botón 'Ingresar nueva ruta' presionado");
+            }
+        });
+        continuarMismaRutaButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null, "Botón 'Continuar con la misma ruta' presionado");
+            }
+        });
+
+        ventanaOpciones.getContentPane().add(buttonPanel);
+        ventanaOpciones.setLocationRelativeTo(null);
+        ventanaOpciones.setVisible(true);
     }
 }
