17f917e992646965e5dd7f1f4e6b971c311aa0a5
Elisa Ajxup Yax <lisajxupyax@gmail.com>
2023-10-09 21:59:37 -0600

    Comentarios

--- PDFFileMetadataReader/src/main/java/com/mycompany/pdffilemetadatareader/PDFFileMetadataReader.java
+++ PDFFileMetadataReader/src/main/java/com/mycompany/pdffilemetadatareader/PDFFileMetadataReader.java
@@ -1,30 +1,38 @@
 package com.mycompany.pdffilemetadatareader;
+
 import com.formdev.flatlaf.FlatDarkLaf;
+
+import java.text.DecimalFormat;
+
 import javax.swing.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.*;
+import java.io.FileOutputStream;
+
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.*;
-import java.io.FileOutputStream;
-import org.apache.pdfbox.pdmodel.PDDocumentInformation;
-import java.io.File;
-import java.io.IOException;
+
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
+
 import org.apache.pdfbox.Loader;
 import org.apache.pdfbox.cos.COSName;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDDocumentInformation;
+import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.PDResources;
+import org.apache.pdfbox.pdmodel.common.PDRectangle;
 import org.apache.pdfbox.pdmodel.font.PDFont;
 import org.apache.pdfbox.pdmodel.font.PDType0Font;
 import org.apache.pdfbox.pdmodel.font.PDType1Font;
 import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject;
-import java.text.DecimalFormat;
-import java.util.Objects;
-import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.pdmodel.PDPage;
-import org.apache.pdfbox.pdmodel.common.PDRectangle;
+
 
 public class PDFFileMetadataReader {
     
@@ -68,8 +76,10 @@ public class PDFFileMetadataReader {
             @Override
             public void actionPerformed(ActionEvent e) {
                 frame.dispose();
+                // Si es la primera vez, se ingresará la ruta
                 if (isFirstTime) {
                     ingresarRuta();
+                // Si no es la primera vez se abrira una ventana con dos opciones
                 } else {
                     mostrarVentanaOpciones();
                 }
@@ -88,6 +98,7 @@ public class PDFFileMetadataReader {
         frame.setVisible(true);
     }
 
+    
     private static void mostrarVentanaOpciones() {
         JFrame ventanaOpciones = new JFrame("Opciones");
         ventanaOpciones.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -145,36 +156,50 @@ public class PDFFileMetadataReader {
         ventanaOpciones.setLocationRelativeTo(null);
         ventanaOpciones.setVisible(true);
     }
+  
     
     private static void ingresarRuta() {
-        // Mostrar un cuadro de diálogo para que el usuario seleccione una carpeta
-        JFileChooser fileChooser = new JFileChooser();
-        fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-        int result = fileChooser.showOpenDialog(null);
-        if (result == JFileChooser.APPROVE_OPTION) {
-            File selectedFolder = fileChooser.getSelectedFile();
-            List<PDFFileInfo> pdfFiles = buscarArchivosPDF(selectedFolder);
-            // Guardar la información en un archivo
-            guardarInformacionEnArchivo(pdfFiles);
-            JOptionPane.showMessageDialog(null, "Búsqueda completada y datos guardados.");
-        }
+    // Mostrar un cuadro de diálogo para que el usuario seleccione una carpeta
+    JFileChooser fileChooser = new JFileChooser();
+    // Configurar el cuadro de diálogo para seleccionar solo directorios (carpetas)
+    fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+    // Mostrar el cuadro de diálogo y esperar a que el usuario seleccione una carpeta
+    int result = fileChooser.showOpenDialog(null);
+    // Verificar si el usuario seleccionó una carpeta
+    if (result == JFileChooser.APPROVE_OPTION) {
+        // Obtener la carpeta seleccionada por el usuario
+        File selectedFolder = fileChooser.getSelectedFile();
+        // Buscar archivos PDF dentro de la carpeta y sus subcarpetas, usando la función buscarArchivosPDF
+        List<PDFFileInfo> pdfFiles = buscarArchivosPDF(selectedFolder);
+        // Guardar la información de los archivos PDF en un archivo, usando la función guardarInformacionEnArchivo
+        guardarInformacionEnArchivo(pdfFiles);
+        // Mostrar un mensaje al usuario para indicar que la búsqueda se ha completado y los datos se han guardado
+        JOptionPane.showMessageDialog(null, "Búsqueda completada y datos guardados.");
     }
+    }
+
     
     private static List<PDFFileInfo> buscarArchivosPDF(File folder) {
     List<PDFFileInfo> pdfFiles = new ArrayList<>();
+    // Llama a la función recursiva para buscar archivos PDF dentro de la carpeta y sus subcarpetas
     buscarArchivosPDFRecursivamente(folder, pdfFiles);
     return pdfFiles;
-    }
+}
 
+    
     private static void buscarArchivosPDFRecursivamente(File folder, List<PDFFileInfo> pdfFiles) {
         if (folder.isDirectory()) {
             File[] files = folder.listFiles();
             if (files != null) {
                 for (File file : files) {
                     if (file.isDirectory()) {
+                        // Si el elemento es una carpeta, llama recursivamente a esta función
                         buscarArchivosPDFRecursivamente(file, pdfFiles);
                     } else if (file.isFile() && file.getName().toLowerCase().endsWith(".pdf")) {
+                        // Si el elemento es un archivo PDF, obtiene su información utilizando la función obtenerInformacionPDF()
                         PDFFileInfo fileInfo = obtenerInformacionPDF(file);
+
+                        // Verifica si la información no es nula antes de agregarla a la lista de archivos PDF encontrados
                         if (fileInfo != null) {
                             pdfFiles.add(fileInfo);
                         }
@@ -184,6 +209,7 @@ public class PDFFileMetadataReader {
         }
     }
 
+    
     private static PDFFileInfo obtenerInformacionPDF(File pdfFile) {
     try {
         PDDocument document = Loader.loadPDF(pdfFile);
@@ -213,21 +239,28 @@ public class PDFFileMetadataReader {
         }
     }
     
+    
     private static List<String> obtenerImagenesDesdePDF(PDDocument document) {
+    // Crea una lista para almacenar las rutas de las imágenes encontradas
     List<String> images = new ArrayList<>();
     try {
+        // Itera a través de todas las páginas del documento PDF
         for (int pageNumber = 0; pageNumber < document.getNumberOfPages(); pageNumber++) {
+            // Obtiene una página específica del PDF
             PDPage page = document.getPage(pageNumber);
+            // Obtiene los recursos de la página, como imágenes y fuentes
             PDResources resources = page.getResources();
+            // Obtiene los nombres de los objetos XObject (que pueden ser imágenes)
             Iterable<COSName> xObjectNames = resources.getXObjectNames();
-
+            // Itera a través de los nombres de objetos XObject
             for (COSName xObjectName : xObjectNames) {
+                // Verifica si el objeto XObject actual es una imagen
                 if (resources.isImageXObject(xObjectName)) {
+                    // Obtiene el objeto XObject de la imagen
                     PDImageXObject imageXObject = (PDImageXObject) resources.getXObject(xObjectName);
-                    // Aquí puedes procesar la imagen, por ejemplo, guardarla o realizar otras operaciones
-                    // Puedes obtener información adicional sobre la imagen, como su formato, ancho, alto, etc.
-                    // PDImageXObject tiene métodos como getSuffix(), getWidth(), getHeight(), etc.
-                    // Agrega el camino de la imagen a la lista
+                    
+                    // Agrega la representación en cadena del objeto COS a la lista de imágenes
+                    // Esto puede ser útil para identificar la imagen, pero normalmente es una referencia interna
                     images.add(imageXObject.getCOSObject().toString());
                 }
             }
@@ -235,51 +268,71 @@ public class PDFFileMetadataReader {
     } catch (IOException e) {
         e.printStackTrace();
     }
+    // Devuelve la lista de rutas de las imágenes encontradas en el PDF
     return images;
     }
+
     
     private static Set<String> extractFontsFromPDF(String pdfFilePath) throws IOException {
-    Set<String> fonts = new HashSet<>();
-    PDDocument document = Loader.loadPDF(new File(pdfFilePath));
-    
+    // Crea un conjunto para almacenar los nombres de las fuentes únicas encontradas en el PDF
+    Set<String> fonts = new HashSet<>();   
+    // Carga el documento PDF desde el archivo especificado
+    PDDocument document = Loader.loadPDF(new File(pdfFilePath));   
+    // Itera a través de todas las páginas del documento PDF
     for (PDPage page : document.getPages()) {
-        PDResources resources = page.getResources();
+        // Obtiene los recursos de la página, que pueden incluir fuentes
+        PDResources resources = page.getResources();      
+        // Obtiene los nombres de las fuentes utilizadas en la página
         for (COSName fontName : resources.getFontNames()) {
+            // Obtiene la fuente asociada al nombre de la fuente
             PDFont font = resources.getFont(fontName);
+            
+            // Verifica si la fuente es de tipo PDType0Font (fuente compuesta) o PDType1Font (fuente simple)
             if (font instanceof PDType0Font) {
                 PDType0Font type0Font = (PDType0Font) font;
+                
+                // Agrega el nombre de la fuente al conjunto de fuentes (fuentes únicas)
                 fonts.add(type0Font.getName());
             } else if (font instanceof PDType1Font) {
                 PDType1Font type1Font = (PDType1Font) font;
+                
+                // Agrega el nombre de la fuente al conjunto de fuentes (fuentes únicas)
                 fonts.add(type1Font.getName());
             }
         }
-    }
-    document.close();
+    }    
+    // Cierra el documento PDF
+    document.close();   
+    // Devuelve el conjunto de nombres de fuentes únicas encontradas en el PDF
     return fonts;
     }
 
+    
     private static String getPageSize(PDDocument file) {
-    DecimalFormat df = new DecimalFormat("###.#");
+    // Crea un objeto DecimalFormat para formatear los valores de tamaño de página
+    DecimalFormat df = new DecimalFormat("###.#"); 
+    // Obtiene la primera página del documento
     PDPage page = file.getPage(0);
-
     String pageSize;
-
+    // Obtiene el rectángulo de medios (mediaBox) de la página
     PDRectangle mediaBox = page.getMediaBox();
-
+    // Calcula el ancho y el alto de la página en pulgadas (dividiendo por 72 puntos por pulgada)
     double width = mediaBox.getWidth() / 72;
     double height = mediaBox.getHeight() / 72;
-
+    // Compara el ancho y el alto con tamaños de página estándar para determinar el tipo de página
     if (((Objects.equals(df.format(width), "8.5")) || ((Objects.equals(df.format(height), "11.0"))))) {
         pageSize = "Carta";
     } else if (((Objects.equals(df.format(width), "8.3")) || ((Objects.equals(df.format(height), "11.8"))))) {
         pageSize = "Oficio";
     } else {
+        // Si no coincide con ningún tamaño estándar, se muestra el ancho y el alto en pulgadas
         pageSize = df.format(width) + " x " + df.format(height);
-    }
+    }  
+    // Devuelve el tamaño de la página como una cadena de texto
     return pageSize;
     }
 
+    
     private static void guardarInformacionEnArchivo(List<PDFFileInfo> pdfFiles) {
     try (ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream("pdfInfo.dat"))) {
         outputStream.writeObject(pdfFiles);
@@ -292,11 +345,11 @@ public class PDFFileMetadataReader {
     }
 }
 
+    
     private static void guardarInformacionEnCSV(List<PDFFileInfo> pdfFiles) {
         try (FileWriter writer = new FileWriter(csvFileName)) {
             // Escribir el encabezado CSV
             writer.write("Nombre,Autor,Tamaño de Archivo (bytes),Tamaño de Página,Páginas,Título,Asunto,Palabras Clave,Tipo de Archivo,Versión de PDF,Aplicación por la que fue creada,Lista de Imágenes en el Documento,Lista de Fuentes en el Documento\n");
-
             // Llenar el archivo CSV con los datos de los archivos PDF
             for (PDFFileInfo fileInfo : pdfFiles) {
                 StringBuilder line = new StringBuilder();
@@ -315,12 +368,12 @@ public class PDFFileMetadataReader {
                 line.append(fileInfo.getFonts()).append(",");
                 writer.write(line.toString() + "\n");
             }
-
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
+    
     private static void continuarMismaRuta() {
     // Cargar la información desde el archivo
     List<PDFFileInfo> pdfFiles = cargarInformacionDesdeArchivo();
